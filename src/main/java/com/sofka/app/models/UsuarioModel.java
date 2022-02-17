03f0d054af619f4df7534233c7c0f37690af30bb
V3RSER <78446571+V3RSER@users.noreply.github.com>
2022-02-17 07:27:17 -0500

    Agregado modelo de usuario
    Se conectó a la base de datos y se estableció el modelo usuario con su respectiva tabla

---
+++ src/main/java/com/sofka/app/models/UsuarioModel.java
@@ -0,0 +1,48 @@
+package com.sofka.app.models;
+
+import javax.persistence.*;
+
+@Entity // Se establece que es un modelo
+@Table(name = "usuario") // Se establece que la tabla asociada al modelo tenga este nombre
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+}
