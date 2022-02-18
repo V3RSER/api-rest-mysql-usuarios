40a0ab91c41e37e4599fc503c7e83f135ea149fd
V3RSER <78446571+V3RSER@users.noreply.github.com>
2022-02-17 21:44:25 -0500

    Agregada consultas de usuario y borrar por id

--- src/main/java/com/sofka/app/repositories/UsuarioRepository.java
+++ src/main/java/com/sofka/app/repositories/UsuarioRepository.java
@@ -4,7 +4,10 @@ import com.sofka.app.models.UsuarioModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository // Se establece que es un repositorio
 public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
 
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
 }
--- src/main/java/com/sofka/app/services/UsuarioService.java
+++ src/main/java/com/sofka/app/services/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service // Se establece que es un sevicio
 public class UsuarioService {
@@ -19,4 +20,23 @@ public class UsuarioService {
     public UsuarioModel guardarUsuario(UsuarioModel usuario) {
         return usuarioRepository.save(usuario);
     }
+
+    public Optional<UsuarioModel> obtenerPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+
+    public ArrayList<UsuarioModel> obtenerPorPrioridad(Integer prioridad) {
+        return usuarioRepository.findByPrioridad(prioridad);
+    }
+
+    public boolean eliminarUsuario(Long id) {
+        try {
+            usuarioRepository.deleteById(id);
+            return true;
+        } catch (Exception err) {
+            return false;
+        }
+    }
+
 }
